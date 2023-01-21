9f84401d9c8f25b424912c579ab2547c8c9da8b4
Santosh Managuli <rocksantosh15@gmail.com>
2023-01-21 10:26:20 +0530

    learnt for..in and sum zero

---
+++ for..in.js
@@ -0,0 +1,11 @@
+// Example of for..in loop in js
+
+let obj = {
+    name: 'Santosh',
+    age: '15',
+    address: 'pune'
+}
+
+for(let key in obj){
+    console.log(obj[key]);
+}
---
+++ sum_zero.js
@@ -0,0 +1,14 @@
+//checking the sun zero problem
+
+function sumZero(arr) {
+    for (const num of arr) {
+        for (let j = 1; j < arr.length - 1; j++) {
+            if (num + arr[j] === 0) {
+                return [num, arr[j]];
+            }
+        }
+    }
+}
+
+const res = sumZero([-5,-4,-3,-2,0,1,2,4,6,8]);
+console.log(res);
